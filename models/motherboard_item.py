9122b7fba5183ac10f4359ffccc96bdcde038e42
vitosgeen <vitosgeen@yandex.ru>
2024-12-03 20:30:36 +0200

    Add .gitignore and implement MotherboardItem model class

---
+++ models/motherboard_item.py
@@ -0,0 +1,23 @@
+class MotherboardItem:
+    def __init__(self, id, orig_id, title, price, link, description, category, manufacturer):
+        self.id = id
+        self.orig_id = orig_id
+        self.title = title
+        self.price = price
+        self.link = link
+        self.description = description
+        self.category = category
+        self.manufacturer = manufacturer
+
+    @classmethod
+    def from_dict(self, data):
+        return MotherboardItem(
+            id=data["id"],
+            orig_id=data["orig_id"],
+            title=data["title"],
+            price=data["price"],
+            link=data["link"],
+            description=data["description"],
+            category=data["category"],
+            manufacturer=data["manufacturer"]
+        )
