24fb156bbafc19703ab8a0cc6fe7490baf1ee4fb
vitosgeen <vitosgeen@yandex.ru>
2024-12-04 09:13:07 +0200

    Refactor motherboard parsing logic to use a dedicated function for adding motherboards to the database

--- parser.py
+++ parser.py
@@ -21,39 +21,27 @@ def start_parser(manufacture, db):
     # start parser based on manufacture type with switch case
     if manufacture.lower() == models.manufacturer.Manufacturer().ASUS.lower():
         motherboards = parsers.asus.motherboard_list.start_parser_moterboard_list()
-        # add to db
-        for motherboard in motherboards:
-            # check if motherboard exists in db by url
-            motherboard_loaded = mbir.getByLink(motherboard.link)
-            if motherboard_loaded is None:
-                mbir.add(motherboard)
+        add_motherboards(motherboards, mbir)
 
     elif manufacture.lower() == models.manufacturer.Manufacturer().MSI.lower():
         motherboards = parsers.msi.motherboard_list.start_parser_moterboard_list()
-        # add to db
-        for motherboard in motherboards:
-            # check if motherboard exists in db by url
-            motherboard_loaded = mbir.getByLink(motherboard.link)
-            if motherboard_loaded is None:
-                mbir.add(motherboard)
+        add_motherboards(motherboards, mbir)
 
     elif manufacture.lower() == models.manufacturer.Manufacturer().GIGABYTE.lower():
         motherboards = parsers.gigabyte.motherboard_list.start_parser_moterboard_list()
-        # add to db
-        for motherboard in motherboards:
-            # check if motherboard exists in db by url
-            motherboard_loaded = mbir.getByLink(motherboard.link)
-            if motherboard_loaded is None:
-                mbir.add(motherboard)
+        add_motherboards(motherboards, mbir)
     elif manufacture.lower() == models.manufacturer.Manufacturer().ASROCK.lower():
         motherboards = parsers.asrock.motherboard_list.start_parser_moterboard_list()
-        # add to db
-        for motherboard in motherboards:
-            # check if motherboard exists in db by url
-            motherboard_loaded = mbir.getByLink(motherboard.link)
-            if motherboard_loaded is None:
-                mbir.add(motherboard)
+        add_motherboards(motherboards, mbir)
 
     else:
         print('Error: manufacture (' + manufacture + ') is not implemented')
         sys.exit(1)
+
+
+def add_motherboards(motherboards, mbir):
+    for motherboard in motherboards:
+        # check if motherboard exists in db by url
+        motherboard_loaded = mbir.getByLink(motherboard.link)
+        if motherboard_loaded is None:
+            mbir.add(motherboard)
