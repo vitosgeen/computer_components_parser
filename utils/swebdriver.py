3011e1572f0854ea257fce042eb070e8fb8e13fd
vitosgeen <vitosgeen@yandex.ru>
2025-01-30 22:19:41 +0200

    Refactor create_driver function to use ChromeDriverManager for driver installation and improve option handling

--- utils/swebdriver.py
+++ utils/swebdriver.py
@@ -2,25 +2,31 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 def create_driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-extensions')
-    options.add_argument("--headless")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument("--headless")
 
     # add option current user data dir
-    options.add_argument('--disable-blink-features=AutomationControlled')
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
     # add user agent
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
     # add download directory
     prefs = {
         "download.prompt_for_download": False,
         "download.directory_upgrade": True,
         "safebrowsing.enabled": True
     }
-    options.add_experimental_option("prefs", prefs)
-    driver = webdriver.Chrome(options=options)
+    chrome_options.add_experimental_option("prefs", prefs)
+    chrome_options.binary_location = "/usr/bin/google-chrome"
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    # driver = webdriver.Chrome(options=options)
     driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
         "source": """
