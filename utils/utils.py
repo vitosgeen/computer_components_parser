e5f41e117554f2152aed9748bed6770767f19d2c
vitosgeen <vitosgeen@yandex.ru>
2025-01-30 22:06:59 +0200

    Add function to remove trailing slash from URLs in utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -9,3 +9,7 @@ def get_origin(url):
     get_origin = urlparse(url).scheme + "://" + urlparse(url).netloc
     return get_origin
     
+def get_url_without_last_slash(url):
+    if url[-1] == "/":
+        return url[:-1]
+    return url
