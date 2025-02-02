1c93dbb5310598741e3b9a60b886924e734ef0c5
vitosgeen <vitosgeen@yandex.ru>
2025-02-02 19:50:45 +0200

    Add function to remove last part of URL for improved URL handling

--- utils/utils.py
+++ utils/utils.py
@@ -13,3 +13,7 @@ def get_url_without_last_slash(url):
     if url[-1] == "/":
         return url[:-1]
     return url
+
+# remove last part of url after slash and return url
+def get_url_without_last_part(url):
+    return url.rsplit('/', 1)[0]
