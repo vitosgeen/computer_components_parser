81ad92979f81146112ce8062c5e57273a2bb7205
vitosgeen <vitosgeen@yandex.ru>
2025-02-05 20:51:59 +0200

    Refactor MSI motherboard parser to improve error handling and enhance content extraction methods

--- parsers/msi/motherboard_page.py
+++ parsers/msi/motherboard_page.py
@@ -1,4 +1,5 @@
 import random
+import time
 from bs4 import BeautifulSoup
 
 from models.manufacturer import Manufacturer
@@ -11,15 +12,26 @@ def start_parser_motherboard_pages(mbir):
     # get all msi motherboard items from db
     motherboard_items = mbir.getAllMotherboardsByManufacturer(Manufacturer().MSI)
     motherboard_overviews_result = []
+    it_was = False
     for motherboard_item in motherboard_items:
+        print("start_parser_motherboard_page: ", motherboard_item.link)
+        # it's for speed up parsing and testing and debugging and development only.
+        # remove this line in production
+        # if motherboard_item.link == "https://www.msi.com/Motherboard/TPM-20-Module":
+        #     it_was = True
+        # if not it_was:
+        #     continue
+        # remove this line in production
         # start parse msi motherboard page
-        motherboard_overviews = start_parser_motherboard_page(motherboard_item)
-        if motherboard_overviews is None:
+        if "Module" in motherboard_item.link:
             continue
-        for motherboard_overview in motherboard_overviews:
-            motherboard_overview.mb_item_id = motherboard_item.id
-            motherboard_overviews_result.append(motherboard_overview)
-    
+        motherboard_overviews = start_parser_motherboard_page(motherboard_item)
+        # if motherboard_overviews is None:
+        #     continue
+        # for motherboard_overview in motherboard_overviews:
+        #     motherboard_overview.mb_item_id = motherboard_item.id
+        #     motherboard_overviews_result.append(motherboard_overview)
+
     return motherboard_overviews_result
         
 def start_parser_motherboard_page(motherboard_item):
@@ -32,16 +44,38 @@ def start_parser_motherboard_page(motherboard_item):
     sleep_delay = random.randint(1, 5)
 
     # get content from overview page like motherboard_item.link
-    content = utils.download.download_file(motherboard_item.link, sleep_time=sleep_delay)
+    content = utils.download.download_file_by_selenium(motherboard_item.link)
     if content is None:
         return
     
+    time.sleep(sleep_delay)
+    
     # parse content from overview page find type link_overview, link_technical_spec, link_support
-    motherboard_overviews += parse_motherboard_overview_links(content, motherboard_item)
-    motherboard_overviews += parse_motherboard_model(content, motherboard_item)
-    motherboard_overviews += parse_motherboard_name(content, motherboard_item)
-    motherboard_overviews += parse_motherboard_description(content, motherboard_item)
-    motherboard_overviews += parse_motherboard_image(content, motherboard_item)
+    motherboard_overviews_links = parse_motherboard_overview_links(content, motherboard_item)
+    if len(motherboard_overviews_links) == 0:
+        print("Error: msi motherboard overview links not found")
+        exit()
+    motherboard_overviews += motherboard_overviews_links
+    motherboard_overviews_model = parse_motherboard_model(content, motherboard_item)
+    if len(motherboard_overviews_model) == 0:
+        print("Error: msi motherboard model not found")
+        exit()
+    motherboard_overviews += motherboard_overviews_model
+    motherboard_overviews_name = parse_motherboard_name(content, motherboard_item)
+    if len(motherboard_overviews_name) == 0:
+        print("Error: msi motherboard name not found")
+        exit()
+    motherboard_overviews += motherboard_overviews_name
+    motherboard_overviews_description = parse_motherboard_description(content, motherboard_item)
+    if len(motherboard_overviews_description) == 0:
+        print("Error: msi motherboard description not found")
+        exit()
+    motherboard_overviews += motherboard_overviews_description
+    motherboard_overviews_image = parse_motherboard_image(content, motherboard_item)
+    if len(motherboard_overviews_image) == 0:
+        print("Error: msi motherboard image not found")
+        exit()
+    motherboard_overviews += motherboard_overviews_image
 
     return motherboard_overviews
 
@@ -75,9 +109,8 @@ def parse_motherboard_overview_links(content, motherboard_item):
                 continue
             if href.find("http") == -1:
                 href = utils.utils.get_origin(motherboard_item.link) + href
+            print("parse_motherboard_overview_links: ", type_item, href)
             motherboard_overview.append(MotherboardOverview(0, motherboard_item.id, type_item, href))
-        
-
 
     if len(motherboard_overview) == 0:
         print("Error: msi motherboard overview links not found")
@@ -97,6 +130,7 @@ def parse_motherboard_name(content, motherboard_item):
             print("Error: msi motherboard name not found (selector: %s)" % selector)
             continue
         for name_element in name_elements:
+            print("parse_motherboard_name: ", name_element.text)
             # if text contains "logo" that just replace it with one space
             if "logo" in name_element.text.lower():
                 name_element.text = name_element.text.replace("logo", " ")
@@ -112,7 +146,7 @@ def parse_motherboard_model(content, motherboard_item):
     motherboard_overview = []
     soup = BeautifulSoup(content, "html.parser")
     models_selectors = [
-        ".productNav__mainNav__title"
+        "h1.productNav__mainNav__title",
     ]
     for selector in models_selectors:
         model_elements = soup.select(selector)
@@ -120,22 +154,8 @@ def parse_motherboard_model(content, motherboard_item):
             print("Error: msi motherboard model not found (selector: %s)" % selector)
             continue
         for model_element in model_elements:
-            class_model = model_element.get("class")
-            if class_model is None:
-                continue
-            # class_model to string
-            class_model = " ".join(class_model)
-            if "modelName" in class_model:
-                motherboard_overview.append(MotherboardOverview(0, motherboard_item.id, MotherboardOverview.TYPE_MODEL, model_element.text))
-                break
+            motherboard_overview.append(MotherboardOverview(0, motherboard_item.id, MotherboardOverview.TYPE_MODEL, model_element.text))
 
-    if len(motherboard_overview) == 0 and motherboard_item.link.find("rog") != -1:
-        return motherboard_overview
-        
-    if len(motherboard_overview) == 0:
-        print("Error: msi motherboard model not found")
-        exit()
-    
     return motherboard_overview
 
 def parse_motherboard_description(content, motherboard_item):
@@ -145,6 +165,15 @@ def parse_motherboard_description(content, motherboard_item):
         ".textContainer",
         ".story__box .story__text",
         ".kv .msiText",
+        ".msi__container p",
+        ".ai__item",
+        ".foucs__feature li",
+        ".KV-main",
+        ".feature-list-item .msiText",
+        "#Product .productslist li",
+        "#product.gaming .list-container ul li",
+        "#mbheder .leftbox li",
+        "#productFeature-section .main-block-content",
     ]
     soup = BeautifulSoup(content, "html.parser")
     motherboard_overview = []
@@ -155,6 +184,9 @@ def parse_motherboard_description(content, motherboard_item):
             print("Error: msi motherboard description not found (selector: %s)" % selector)
             continue
         for description_element in description_elements:
+            # check length of text
+            if len(description_element.text) < 100:
+                continue
             motherboard_overview.append(MotherboardOverview(0, motherboard_item.id, MotherboardOverview.TYPE_DESCRIPTION, description_element.text))
 
     if len(motherboard_overview) == 0 and motherboard_item.link.find("rog") != -1:
@@ -168,7 +200,13 @@ def parse_motherboard_image(content, motherboard_item):
     image_selectors = [
         "#product picture img",
         ".dashBoard__figure img",
-        ".kv img",        
+        ".kv img",
+        "#pd-slideshow img",
+        "div.img img",
+        "#Product .slick-track img",
+        "#mbheder img.io",
+        ".main-block-category .main-img img",
+        ".main-block-container .main-block-img img",
     ]
     soup = BeautifulSoup(content, "html.parser")
     motherboard_overview = []
@@ -191,12 +229,10 @@ def parse_motherboard_image(content, motherboard_item):
                 src = image_element.get("src")
             else:
                 continue
+
+            # if src is started with // add https: to src
+            if src.find("//") == 0:
+                src = "https:" + src
+
             motherboard_overview.append(MotherboardOverview(0, motherboard_item.id, MotherboardOverview.TYPE_IMAGE, src))
-
-
-
-    if len(motherboard_overview) == 0:
-        print("Error: msi motherboard image not found")
-        exit()
-
     return motherboard_overview
