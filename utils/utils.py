ea2edf25973bd4708bc79bcb3f6e637169853f37
vitosgeen <vitosgeen@yandex.ru>
2025-02-13 17:39:09 +0200

    Add utility functions for URL manipulation to enhance URL processing capabilities

--- utils/utils.py
+++ utils/utils.py
@@ -17,3 +17,16 @@ def get_url_without_last_slash(url):
 # remove last part of url after slash and return url
 def get_url_without_last_part(url):
     return url.rsplit('/', 1)[0]
+
+def get_url_without_first_part(url):
+    return url.split('/', 1)[1]
+
+def get_url_without_first_and_last_part(url):
+    return get_url_without_last_part(get_url_without_first_part(url))
+
+def get_last_part_from_url(url):
+    return url.rsplit('/', 1)[1]
+
+def get_part_before_last_from_url(url):
+    url_without_last_part = get_url_without_last_part(url)
+    return get_last_part_from_url(url_without_last_part)
